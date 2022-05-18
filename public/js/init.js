1124df9b94ee2efcd8515d9be2ad4ad38851e8b9
youthred <xxooec@gmail.com>
2022-05-18 21:56:45 +0800

    icon

--- public/js/init.js
+++ public/js/init.js
@@ -7,6 +7,14 @@ window.onload = function () {
         if (req.status === 200) {
             const conf = JSON.parse(req.responseText)
             document.title = conf.title
+            setIcon(conf.favicon_url)
         }
     }
+}
+
+function setIcon(favicon_url) {
+    let link = document.createElement('link')
+    link.rel = 'icon'
+    link.href = favicon_url
+    document.getElementsByTagName('head')[0].appendChild(link)
 }
